108bb7934c647c92eb24aacda52105e2bf5dee76
leticinha777 <leticiabsb1234@gmail.com>
2025-11-11 16:10:49 -0300

    Add files via upload

--- eventos.c
+++ eventos.c
@@ -3,6 +3,12 @@
 #include <math.h>
 #include "eventos.h"
 
+float distancia (struct coordenada b, struct coordenada d)
+{
+    //calcula a distância entre dois ponos usando pitágoras, retorna a distância como um float
+    return sqrt((b.x - d.x)*(b.x - d.x) + (b.y - d.y)*(b.y - d.y));
+}
+
 struct evento *cria_evento (int time, int heroi, int base, int missao)
 {
     struct evento *e;
@@ -38,6 +44,9 @@ void chega (struct mundo *w, int t,int h,int b)
     
     e = cria_evento(t,h,b,-1);
 
+    if(!e)
+        return;
+
     if(espera)
     {
         fprio_insere(w->lef, e, ESPERA, e->time);
@@ -67,6 +76,9 @@ void espera (struct mundo *w, int t, int h, int b)
     maior_fila(w, b); // faz a atualização do maior valor da fila de espera
 
     e = cria_evento(t,-1,b,-1);
+
+    if(!e)
+        return;
     fprio_insere(w->lef, e, AVISA, t);
 
     printf("%6d: ESPERA HEROI %2d BASE %d (%2d)\n", t, h, b, aux);
@@ -81,6 +93,10 @@ void desiste (struct mundo *w, int t, int h, int b)
         return;
     
     e = cria_evento(t,h,(aleat(0,w->n_bases -1)),-1);
+
+    if(!e)
+        return;
+
     fprio_insere(w->lef, e, VIAJA, t);
 
     printf("%6d: DESIST HEROI %2d BASE %d\n", t, h, b);
@@ -105,6 +121,10 @@ void avisa (struct mundo *w, int t, int b)
         cjto_insere(w->vbases[b].pres, h);
 
         e = cria_evento(t,h,b,-1);
+
+        if(!e)
+            return;
+
         fprio_insere(w->lef, e, ENTRA, t);
 
         printf("%6d: AVISA  PORTEIRO BASE %d ADMITE %2d\n", t, b, h);
@@ -123,6 +143,10 @@ void entra (struct mundo *w, int t, int h, int b)
     tpb = 15 + w->vherois[h].paciencia * aleat(1, 20);
 
     e = cria_evento(t+tpb, h, b, -1);
+
+    if(!e)
+        return;
+
     fprio_insere(w->lef, e, SAI, e->time);
 
     printf("%6d: ENTRA  HEROI %2d BASE %d (%2d/%2d) SAI %d\n", t, h, b, cjto_card(w->vbases[b].pres), w->vbases[b].lot, e->time);
@@ -138,20 +162,18 @@ void sai (struct mundo *w, int t, int h, int b)
 
     cjto_retira(w->vbases[b].pres, h);
 
+    e = cria_evento(t,h,aleat(0, w->n_bases -1), -1);
     
-    e->time = t;
-    e->heroi = h;
-    e->base = aleat(0, w->n_bases - 1);
-    e->missao = -1;
-    fprio_insere(w->lef, e, VIAJA, t);
-
-    if(!(e = malloc(sizeof(struct evento))))
+    if(!e)
         return;
 
-    e->time = t;
-    e->heroi = -1;
-    e->base = b;
-    e->missao = -1;
+    fprio_insere(w->lef, e, VIAJA, t);
+
+    e = cria_evento(t,-1,b,-1);
+
+    if(!e)
+        return;
+
     fprio_insere(w->lef, e, AVISA, t);
 
     printf("%6d: SAI HEROI %2d BASE %d (%2d/%2d)\n", t, h, b, cjto_card(w->vbases[b].pres), w->vbases[b].lot);
@@ -159,15 +181,72 @@ void sai (struct mundo *w, int t, int h, int b)
 
 //Função que trata o evento VIAJA
 void viaja (struct mundo *w, int t, int h, int b)
+{
+    int b_agora, duracao, velocidade;
+    float distancia_a;
+    struct evento *e;
+
+    if(w->vherois[h].vida == 1)
+        return;
+
+    b_agora = w->vherois[h].base;
+    distancia_a = distancia(w->vbases[b_agora].local, w->vbases[b].local);
+    velocidade = w->vherois[h].velocidade;
+    duracao = distancia_a / velocidade;
+    w->vherois[h].base = -1;
+
+    e = cria_evento(t+duracao, h, b, -1);
+    
+    if(!e)
+        return;
+    fprio_insere(w->lef, e, CHEGA, e->time);
+
+    printf("%6d: VIAJA  HEROI %2d BASE %d BASE %d DIST %f VEL %d CHEGA %d\n", t, h, b_agora, b, distancia_a, velocidade, e->time);
+}
 
 /* O herói H morre no instante T */
 void morre (struct mundo *w, int t, int h, int b, int ms)
+{
+    struct evento *e;
+
+    if(w->vherois->vida)
+        return;
+
+    w->vherois[h].vida = 1;
+    w->vherois[h].base = -1;
+    if(b >=0 && b < w->n_bases && w->vbases[b].pres != NULL)
+    cjto_retira(w->vbases[b].pres, h);
+
+    e = cria_evento(t,-1,b,-1);
+
+    if(!e)
+        return;
+
+    fprio_insere(w->lef, e, AVISA, t);
+
+    printf("%6d: MORRE  HEROI %2d MISSAO %d\n", t, h, missao);
+}
 
 //Função usada para calcular as bases mais proximas da missão
 int base_mais_proxima (struct mundo *w, int t, int missao)
+{
+    for (int i = 0; i < w->n_missoes; i++)
+    {
+        w->vmissoes[i].distancia[i]
+    }
+
+  // ordena o vetor com as distãncias entre as bases, de maneira crescente
+  insertion_sort(d, w->nbases);
+}
 
 //Função que trata o evento MISSAO
 void missao (struct mundo *w, int t, int ms)
+{
+
+}
 
 //Função que trata o evento FIM, imprime as estatísticas finais da simulação
 void fim (struct mundo *w, int t, int qev)
+{
+
+}
